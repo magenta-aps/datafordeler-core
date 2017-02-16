c5d69026988da92913fa2fd8d4e31930ad735479
Lars Peter Thomsen <larsp@magenta.dk>
2017-02-16 12:58:54 +0100

    Part of the code skeleton for plugins

---
+++ src/main/java/dk/magenta/datafordeler/core/Checksum.java
@@ -0,0 +1,12 @@
+package dk.magenta.datafordeler.core;
+
+import java.io.Serializable;
+
+/**
+ * Created by lars on 16-02-17.
+ */
+public class Checksum implements Serializable {
+    public String objectID;
+    public int sequenceNumber;
+    public String checksum;
+}
