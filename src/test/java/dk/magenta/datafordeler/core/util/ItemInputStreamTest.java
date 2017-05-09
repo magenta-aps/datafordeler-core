527440209bbeeeba5d65c2b7f94653b5843b9416
Lars Peter Thomsen <larsp@magenta.dk>
2017-05-09 09:42:42 +0200

    ItemInputStream test

--- src/test/java/dk/magenta/datafordeler/core/util/ItemInputStreamTest.java
+++ src/test/java/dk/magenta/datafordeler/core/util/ItemInputStreamTest.java
@@ -42,7 +42,25 @@ public class ItemInputStreamTest {
 
     @Test
     public void parseTest2() throws IOException {
-        String inputText = "{\"items\":[{\"type\":\"TestObject\",\"foo\":42,\"bar\":\"foobar\"},{\"type\":\"TestObject\",\"foo\":24,\"bar\":\"barfoo\"}]}";
+        String inputText = "{\"items\":[{\"type\":\"TestObject\",\"foo\":42,\"bar\":\"foobar\"},{\"type\":\"TestObject\",\"foo\":24,\"bar\":\"barfoo\"},{\"type\":\"NotPresent\"}]}";
+        InputStream jsonStream = new ByteArrayInputStream(inputText.getBytes());
+        HashMap<String, Class<? extends TestObject>> classMap = new HashMap<>();
+        classMap.put("TestObject", TestObject.class);
+        ItemInputStream<TestObject> stream = ItemInputStream.parseJsonStream(jsonStream, classMap, "items", "type", this.objectMapper);
+        TestObject item;
+        item = stream.next();
+        Assert.assertEquals(42, item.foo);
+        Assert.assertEquals("foobar", item.bar);
+        item = stream.next();
+        Assert.assertEquals(24, item.foo);
+        Assert.assertEquals("barfoo", item.bar);
+        item = stream.next();
+        Assert.assertNull(item);
+    }
+
+    @Test
+    public void parseTest3() throws IOException {
+        String inputText = "{\"container\":{\"items\":[{\"type\":\"TestObject\",\"foo\":42,\"bar\":\"foobar\"},{\"type\":\"TestObject\",\"foo\":24,\"bar\":\"barfoo\"},{\"type\":\"NotPresent\"}]}}";
         InputStream jsonStream = new ByteArrayInputStream(inputText.getBytes());
         HashMap<String, Class<? extends TestObject>> classMap = new HashMap<>();
         classMap.put("TestObject", TestObject.class);
