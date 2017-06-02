8c268b8aaf6f840a33ea4e2742f75c5efdf1d637
Lars Peter Thomsen <larsp@magenta.dk>
2017-06-02 15:05:35 +0200

    Unit testing

--- src/test/java/dk/magenta/datafordeler/plugindemo/model/DemoData.java
+++ src/test/java/dk/magenta/datafordeler/plugindemo/model/DemoData.java
@@ -2,8 +2,11 @@ package dk.magenta.datafordeler.plugindemo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dk.magenta.datafordeler.core.database.DataItem;
+import dk.magenta.datafordeler.core.database.Identification;
 
 import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -25,6 +28,9 @@ public class DemoData extends DataItem<DemoEffect, DemoData> {
     @Column
     @JsonProperty("bynavn")
     private String bynavn;
+
+    @ManyToOne(optional = true, fetch = FetchType.EAGER)
+    private Identification reference;
 
     public DemoData() {}
 
@@ -50,6 +56,27 @@ public class DemoData extends DataItem<DemoEffect, DemoData> {
         HashMap<String, Object> map = new HashMap<>();
         map.put("postnr", this.postnr);
         map.put("bynavn", this.bynavn);
+        map.put("reference", this.reference);
         return map;
+    }
+
+    public HashMap<String, Identification> getReferences() {
+        HashMap<String, Identification> references = super.getReferences();
+        references.put("reference", this.reference);
+        return references;
+    }
+
+    public void updateReferences(HashMap<String, Identification> references) {
+        if (references.containsKey("reference")) {
+            this.reference = references.get("reference");
+        }
+    }
+
+    public Identification getReference() {
+        return reference;
+    }
+
+    public void setReference(Identification reference) {
+        this.reference = reference;
     }
 }
