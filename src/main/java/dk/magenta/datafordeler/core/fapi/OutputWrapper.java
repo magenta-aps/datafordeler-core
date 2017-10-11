ba27e653ee5e153870b2ff58c433c7250e3555f9
Lars Peter Thomsen <larsp@magenta.dk>
2017-10-11 10:16:11 +0200

    Add often-used NodeWrapper to core

--- src/main/java/dk/magenta/datafordeler/core/fapi/OutputWrapper.java
+++ src/main/java/dk/magenta/datafordeler/core/fapi/OutputWrapper.java
@@ -1,5 +1,7 @@
 package dk.magenta.datafordeler.core.fapi;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import dk.magenta.datafordeler.core.database.Entity;
 
 import java.util.ArrayList;
@@ -19,5 +21,55 @@ public abstract class OutputWrapper<E extends Entity> {
             result.add(wrapResult(item));
         }
         return result;
+    }
+
+
+
+    public static class NodeWrapper {
+        private ObjectNode node;
+
+        public NodeWrapper(ObjectNode node) {
+            this.node = node;
+        }
+
+        public ObjectNode getNode() {
+            return this.node;
+        }
+
+        public void put(String key, Boolean value) {
+            if (value != null) {
+                this.node.put(key, value);
+            }
+        }
+        public void put(String key, Short value) {
+            if (value != null) {
+                this.node.put(key, value);
+            }
+        }
+        public void put(String key, Integer value) {
+            if (value != null) {
+                this.node.put(key, value);
+            }
+        }
+        public void put(String key, Long value) {
+            if (value != null) {
+                this.node.put(key, value);
+            }
+        }
+        public void put(String key, String value) {
+            if (value != null) {
+                this.node.put(key, value);
+            }
+        }
+        public void set(String key, JsonNode value) {
+            if (value != null) {
+                this.node.set(key, value);
+            }
+        }
+        public void putPOJO(String key, Object value) {
+            if (value != null) {
+                this.node.putPOJO(key, value);
+            }
+        }
     }
 }
