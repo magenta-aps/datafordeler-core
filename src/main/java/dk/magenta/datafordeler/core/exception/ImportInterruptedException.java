e5c3429e837e90f08c37c2fd59408936215f22c1
Lars Peter Thomsen <larsp@magenta.dk>
2017-12-13 09:18:42 +0100

    Let ImportInterruptedException accept any cause Exception

--- src/main/java/dk/magenta/datafordeler/core/exception/ImportInterruptedException.java
+++ src/main/java/dk/magenta/datafordeler/core/exception/ImportInterruptedException.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ImportInterruptedException extends DataFordelerException {
 
-    public ImportInterruptedException(InterruptedException cause) {
+    public ImportInterruptedException(Exception cause) {
         super(cause);
     }
 
