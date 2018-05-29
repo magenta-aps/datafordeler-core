6e996abba59ca5dd4a836ba5e73c9008dce36533
Lars Peter Thomsen <larsp@magenta.dk>
2018-05-29 14:16:43 +0200

    Formatting

--- src/main/java/dk/magenta/datafordeler/core/fapi/Query.java
+++ src/main/java/dk/magenta/datafordeler/core/fapi/Query.java
@@ -278,7 +278,8 @@ public abstract class Query<E extends Entity> {
         LookupDefinition lookupDefinition = new LookupDefinition(this, this.getDataClass());
         if (this.recordAfter != null) {
             lookupDefinition.put(DataItem.DB_FIELD_LAST_UPDATED, this.recordAfter, OffsetDateTime.class, LookupDefinition.Operator.GT);
-        }if (uuid != null && !uuid.isEmpty()) {
+        }
+        if (uuid != null && !uuid.isEmpty()) {
             lookupDefinition.put(LookupDefinition.entityref + LookupDefinition.separator + "identification" + LookupDefinition.separator + Identification.DB_FIELD_UUID, this.uuid, UUID.class, LookupDefinition.Operator.EQ);
         }
         return lookupDefinition;
