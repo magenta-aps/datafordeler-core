761c7f02bfec67c43535e46217610e81c100c4b5
Lars Peter Thomsen <larsp@magenta.dk>
2018-09-05 12:39:50 +0200

    Http communicator with client certificates

--- src/main/java/dk/magenta/datafordeler/core/plugin/Communicator.java
+++ src/main/java/dk/magenta/datafordeler/core/plugin/Communicator.java
@@ -1,5 +1,6 @@
 package dk.magenta.datafordeler.core.plugin;
 
+import dk.magenta.datafordeler.core.exception.DataFordelerException;
 import dk.magenta.datafordeler.core.exception.DataStreamException;
 import dk.magenta.datafordeler.core.exception.HttpStatusException;
 import org.apache.http.StatusLine;
@@ -12,6 +13,6 @@ public interface Communicator {
 
   InputStream fetch(URI uri) throws HttpStatusException, DataStreamException;
 
-  StatusLine send(URI endpoint, String payload) throws IOException;
+  StatusLine send(URI endpoint, String payload) throws IOException, DataFordelerException;
 
 }
