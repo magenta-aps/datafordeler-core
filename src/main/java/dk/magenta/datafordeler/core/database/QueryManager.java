cafc172fedb619b95119e625579fb26e45fdecc1
Lars Peter Thomsen <larsp@magenta.dk>
2018-09-26 13:43:34 +0200

    Better scan-scroll data handling

--- src/main/java/dk/magenta/datafordeler/core/database/QueryManager.java
+++ src/main/java/dk/magenta/datafordeler/core/database/QueryManager.java
@@ -207,7 +207,6 @@ public abstract class QueryManager {
 
         for (String key : extraParameters.keySet()) {
             Object value = extraParameters.get(key);
-            System.out.println(key+" = "+value);
             if (value instanceof Collection) {
                 databaseQuery.setParameterList(key, (Collection) value);
             } else {
