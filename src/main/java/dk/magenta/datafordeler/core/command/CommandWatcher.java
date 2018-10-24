1eac344a03b44da5776a47e8c2dc0082623ecc24
Lars Peter Thomsen <larsp@magenta.dk>
2018-10-24 15:13:11 +0200

    Name server and match it with incoming command data

--- src/main/java/dk/magenta/datafordeler/core/command/CommandWatcher.java
+++ src/main/java/dk/magenta/datafordeler/core/command/CommandWatcher.java
@@ -8,6 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
