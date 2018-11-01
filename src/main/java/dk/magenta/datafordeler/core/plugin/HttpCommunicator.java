28d90d4f7e42e00d73239a861d635ad3563b88db
Lars Peter Thomsen <larsp@magenta.dk>
2018-11-01 14:33:24 +0100

    More fetch options

--- src/main/java/dk/magenta/datafordeler/core/plugin/HttpCommunicator.java
+++ src/main/java/dk/magenta/datafordeler/core/plugin/HttpCommunicator.java
@@ -3,21 +3,28 @@ package dk.magenta.datafordeler.core.plugin;
 
 import dk.magenta.datafordeler.core.exception.DataStreamException;
 import dk.magenta.datafordeler.core.exception.HttpStatusException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.*;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Very basic Communicator that fetches a resource over HTTP,
@@ -44,8 +51,17 @@ public class HttpCommunicator implements Communicator {
 
     @Override
     public InputStream fetch(URI uri) throws HttpStatusException, DataStreamException {
+        return this.get(uri, null);
+    }
+
+    public InputStream get(URI uri, Map<String, String> headers) throws DataStreamException, HttpStatusException {
         CloseableHttpClient httpclient = this.buildClient();
         HttpGet get = new HttpGet(uri);
+        if (headers != null) {
+            for (String key : headers.keySet()) {
+                get.setHeader(key, headers.get(key));
+            }
+        }
         CloseableHttpResponse response;
         try {
             response = httpclient.execute(get);
@@ -56,8 +72,40 @@ public class HttpCommunicator implements Communicator {
         if (statusLine.getStatusCode() != 200) {
             throw new HttpStatusException(statusLine, uri);
         }
-
         try {
+            return response.getEntity().getContent();
+        } catch (IOException e) {
+            throw new DataStreamException(e);
+        }
+    }
+
+    public InputStream post(URI uri, Map<String, String> parameters, Map<String, String> headers) throws DataStreamException, HttpStatusException, UnsupportedEncodingException {
+        ArrayList<NameValuePair> paramList = new ArrayList<>();
+        for (String key : parameters.keySet()) {
+            paramList.add(new BasicNameValuePair(key, parameters.get(key)));
+        }
+        return this.post(
+                uri,
+                new UrlEncodedFormEntity(paramList),
+                headers
+        );
+    }
+
+    public InputStream post(URI uri, HttpEntity body, Map<String, String> headers) throws DataStreamException, HttpStatusException {
+        CloseableHttpClient httpclient = this.buildClient();
+        HttpPost post = new HttpPost(uri);
+        if (headers != null) {
+            for (String key : headers.keySet()) {
+                post.setHeader(key, headers.get(key));
+            }
+        }
+        post.setEntity(body);
+        try {
+            HttpResponse response = httpclient.execute(post);
+            StatusLine statusLine = response.getStatusLine();
+            if (statusLine.getStatusCode() != 200) {
+                throw new HttpStatusException(statusLine, uri);
+            }
             return response.getEntity().getContent();
         } catch (IOException e) {
             throw new DataStreamException(e);
