cb2360dcb0e392220e44b815be2c0c3e4b43ba77
Lars Peter Thomsen <larsp@magenta.dk>
2019-01-15 12:33:00 +0100

    Pitu login

---
+++ src/main/java/dk/magenta/datafordeler/core/user/PituDafoUserDetails.java
@@ -0,0 +1,140 @@
+package dk.magenta.datafordeler.core.user;
+
+import dk.magenta.datafordeler.core.exception.InvalidCertificateException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+public class PituDafoUserDetails extends DafoUserDetails {
+
+    public static String HEADER_SSL_CLIENT_SUBJECT_DN = "ssl_client_s_dn";
+    public static String HEADER_SSL_CLIENT_ISSUER_DN = "sl_client_i_dn";
+    public static String HEADER_SSL_SERVER_SUBJECT_DN_OU = "ssl_server_s_dn_ou";
+    public static String HEADER_SSL_CLIENT_VERIFY = "ssl_client_verify";
+
+
+    public static String PARAMETER_XROAD_INSTANCE = "xRoadInstance";
+    public static String PARAMETER_MEMBERCLASS = "memberClass";
+    public static String PARAMETER_MEMBERCODE = "memberCode";
+    public static String PARAMETER_SUBSYSTEMCODE = "subsystemCode";
+    public static String PARAMETER_SERVICECODE = "serviceCode";
+    public static String PARAMETER_SERVICEVERSION = "serviceVersion";
+
+    private HashMap<String, UserProfile> userProfiles = new HashMap<>();
+    private HashMap<String, ArrayList<UserProfile>> systemRoles = new HashMap<>();
+
+    String serverSubject;
+    String verify;
+
+    private String nameQualifier;
+    private String onBehalfOf;
+    private String clientSubject;
+
+    private String xRoadInstance;
+    private String memberClass;
+    private String memberCode;
+    private String subsystemCode;
+    private String serviceCode;
+    private String serviceVersion;
+
+    public PituDafoUserDetails(HttpServletRequest request) throws InvalidCertificateException {
+        this(extractHeaders(request), request.getParameterMap());
+    }
+
+
+    private PituDafoUserDetails(Map<String, String> headers, Map<String, String[]> parameters) throws InvalidCertificateException {
+        super();
+        this.clientSubject = headers.get(HEADER_SSL_CLIENT_SUBJECT_DN);
+        this.nameQualifier = headers.get(HEADER_SSL_CLIENT_ISSUER_DN);
+        this.serverSubject = headers.get(HEADER_SSL_SERVER_SUBJECT_DN_OU);
+        this.verify = headers.get(HEADER_SSL_CLIENT_VERIFY);
+
+        Map<String, String> parameterMap = firstParameter(parameters);
+
+        if (this.clientSubject == null || this.clientSubject.isEmpty()) {
+            throw new InvalidCertificateException("Missing certificate header for subject");
+        }
+        if (!"SUCCESS".equals(this.verify)) {
+            throw new InvalidCertificateException("Certificate validation failed. Client verification status was: "+this.verify);
+        }
+
+        this.xRoadInstance = parameterMap.get(PARAMETER_XROAD_INSTANCE);
+        this.memberClass = parameterMap.get(PARAMETER_MEMBERCLASS);
+        this.memberCode = parameterMap.get(PARAMETER_MEMBERCODE);
+        this.subsystemCode = parameterMap.get(PARAMETER_SUBSYSTEMCODE);
+        this.serviceCode = parameterMap.get(PARAMETER_SERVICECODE);
+        this.serviceVersion = parameterMap.get(PARAMETER_SERVICEVERSION);
+    }
+
+    private static Map<String, String> extractHeaders(HttpServletRequest request) {
+        HashMap<String, String> headers = new HashMap<>();
+        for (Enumeration<String> headerNames = request.getHeaderNames(); headerNames.hasMoreElements(); ) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            headers.put(headerName, headerValue);
+        }
+        return headers;
+    }
+
+    private static Map<String, String> firstParameter(Map<String, String[]> parameters) {
+        HashMap<String, String> map = new HashMap<>();
+        for (String name : parameters.keySet()) {
+            String[] values = parameters.get(name);
+            String value = null;
+            if (values != null && values.length > 0) {
+                value = values[0];
+            }
+            map.put(name, value);
+        }
+        return map;
+    }
+
+    @Override
+    public String getNameQualifier() {
+        return this.nameQualifier;
+    }
+
+    @Override
+    public String getIdentity() {
+        return String.format("%s:%s:%s:%s", this.xRoadInstance, this.memberClass, this.memberCode, this.subsystemCode);
+    }
+
+    @Override
+    public String getOnBehalfOf() {
+        return this.onBehalfOf;
+    }
+
+    @Override
+    public boolean isAnonymous() {
+        return false;
+    }
+
+    @Override
+    public boolean hasSystemRole(String role) {
+        return systemRoles.containsKey(role);
+    }
+
+    @Override
+    public boolean hasUserProfile(String userProfileName) {
+        return userProfiles.containsKey(userProfileName);
+    }
+
+    @Override
+    public Collection<String> getUserProfiles() {
+        return userProfiles.keySet();
+    }
+
+    @Override
+    public Collection<String> getSystemRoles() {
+        return systemRoles.keySet();
+    }
+
+    @Override
+    public Collection<UserProfile> getUserProfilesForRole(String role) {
+        if (systemRoles.containsKey(role)) {
+            return systemRoles.get(role);
+        } else {
+            return Collections.EMPTY_LIST;
+        }
+    }
+}
