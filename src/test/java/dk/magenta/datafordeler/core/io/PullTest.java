c4666da3359b3d89e68ba42093aff94701d259ea
madsmj <madsmj@gmail.com>
2019-06-17 07:09:17 +0200

    fix outdated tests

--- src/test/java/dk/magenta/datafordeler/core/io/PullTest.java
+++ src/test/java/dk/magenta/datafordeler/core/io/PullTest.java
@@ -3,17 +3,12 @@ package dk.magenta.datafordeler.core.io;
 import dk.magenta.datafordeler.core.Application;
 import dk.magenta.datafordeler.core.Engine;
 import dk.magenta.datafordeler.core.PluginManager;
-import dk.magenta.datafordeler.core.Pull;
-import dk.magenta.datafordeler.core.exception.DataFordelerException;
 import dk.magenta.datafordeler.core.gapi.GapiTestBase;
 import dk.magenta.datafordeler.core.plugin.Plugin;
 import dk.magenta.datafordeler.core.plugin.RegisterManager;
 import dk.magenta.datafordeler.core.plugin.TaskListener;
-import dk.magenta.datafordeler.core.testutil.ExpectorCallback;
 import dk.magenta.datafordeler.core.testutil.Order;
 import dk.magenta.datafordeler.core.testutil.OrderedRunner;
-import dk.magenta.datafordeler.plugindemo.DemoRegisterManager;
-import dk.magenta.datafordeler.plugindemo.model.DemoEntityRecord;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,15 +20,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.io.IOException;
 import java.time.OffsetDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringJoiner;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @RunWith(OrderedRunner.class)
 @ContextConfiguration(classes = Application.class)
@@ -49,46 +36,11 @@ public class PullTest extends GapiTestBase {
     @LocalServerPort
     private int port;
 
-    @Test
-    @Order(order=1)
-    public void pull() throws DataFordelerException, IOException, InterruptedException, ExecutionException, TimeoutException {
-        Plugin plugin = pluginManager.getPluginForSchema(DemoEntityRecord.schema);
-        DemoRegisterManager.setPortOnAll(this.port);
-
-        String checksum = this.hash(UUID.randomUUID().toString());
-        String reference = "http://localhost:"+this.port+"/test/get/" + checksum;
-        String uuid = UUID.randomUUID().toString();
-        String full = this.getPayload("/referencelookuptest.json")
-                .replace("%{checksum}", checksum)
-                .replace("%{entityid}", uuid)
-                .replace("%{sequenceNumber}", "1");
-
-        ExpectorCallback lookupCallback = new ExpectorCallback();
-        this.callbackController.addCallbackResponse("/test/get/" + checksum, full, lookupCallback);
-
-        String event1 = this.envelopReference("Postnummer", reference);
-        String body = this.jsonList(Collections.singletonList(event1), "events");
-        ExpectorCallback eventCallback = new ExpectorCallback();
-        this.callbackController.addCallbackResponse("/test/getNewEvents", body, eventCallback);
-
-        ExpectorCallback receiptCallback = new ExpectorCallback();
-        this.callbackController.addCallbackResponse("/test/receipt", "", receiptCallback);
-
-        Pull pull = new Pull(engine, plugin);
-        pull.run();
-        //engine.fetchEvents(plugin);
-
-        Assert.assertTrue(lookupCallback.get(20, TimeUnit.SECONDS));
-        Assert.assertTrue(receiptCallback.get(20, TimeUnit.SECONDS));
-
-        this.deleteEntity(uuid);
-        DemoRegisterManager.setPortOnAll(Application.servicePort);
-    }
 
     @Test
     @Order(order=2)
     public void schedule() throws SchedulerException, InterruptedException {
-        Plugin plugin = pluginManager.getPluginForSchema("Postnummer");
+        Plugin plugin = pluginManager.getPluginForSchema("demo");
         RegisterManager registerManager = plugin.getRegisterManager();
         String registerManagerId = registerManager.getClass().getName() + registerManager.hashCode();
 
@@ -134,48 +86,4 @@ public class PullTest extends GapiTestBase {
         Assert.assertEquals(0, taskListener.size(TaskListener.Event.jobWasExecuted));
     }
 
-    @Test
-    @Order(order=3)
-    public void full() throws DataFordelerException, IOException, InterruptedException, ExecutionException, TimeoutException {
-        Plugin plugin = pluginManager.getPluginForSchema("Postnummer");
-        DemoRegisterManager.setPortOnAll(this.port);
-        String checksum = this.hash(UUID.randomUUID().toString());
-        String reference = "http://localhost:"+this.port+"/test/get/" + checksum;
-        String uuid = UUID.randomUUID().toString();
-        String full = this.getPayload("/referencelookuptest.json")
-                .replace("%{checksum}", checksum)
-                .replace("%{entityid}", uuid)
-                .replace("%{sequenceNumber}", "1");
-
-        ExpectorCallback lookupCallback = new ExpectorCallback();
-        this.callbackController.addCallbackResponse("/test/get/" + checksum, full, lookupCallback);
-
-        String event1 = this.envelopReference("Postnummer", reference);
-        String body = this.jsonList(Collections.singletonList(event1), "events");
-        ExpectorCallback eventCallback = new ExpectorCallback();
-        this.callbackController.addCallbackResponse("/test/getNewEvents", body, eventCallback);
-
-        ExpectorCallback receiptCallback = new ExpectorCallback();
-        this.callbackController.addCallbackResponse("/test/receipt", "", receiptCallback);
-
-        OffsetDateTime next = OffsetDateTime.now().plusSeconds(2);
-        String cronSchedule = next.getSecond() + " " + next.getMinute() + " " + next.getHour() + " " + next.getDayOfMonth() + " " + next.getMonthValue() + " " + "?";
-
-        DemoRegisterManager registerManager = (DemoRegisterManager) plugin.getRegisterManager();
-        engine.setupPullSchedule(registerManager, cronSchedule, false);
-
-        Assert.assertTrue(lookupCallback.get(20, TimeUnit.SECONDS));
-        Assert.assertTrue(receiptCallback.get(20, TimeUnit.SECONDS));
-
-        this.deleteEntity(uuid);
-        DemoRegisterManager.setPortOnAll(Application.servicePort);
-    }
-
-    private String jsonList(List<String> jsonData, String listKey) {
-        StringJoiner sj = new StringJoiner(",");
-        for (String j : jsonData) {
-            sj.add(j);
-        }
-        return "{\""+listKey+"\":["+sj.toString()+"]}";
-    }
 }
